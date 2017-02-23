4410090d5883da544a0a997c8bb39b904bb36066
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-23 09:32:55 -0700

    Passing test for API index
    - add serializer
    - add routes
    - add items_controller

--- spec/requests/api/v1/items_api_spec.rb
+++ spec/requests/api/v1/items_api_spec.rb
@@ -10,11 +10,14 @@ describe "Items API" do
                           description: "everyone needs two",
                           image_url: "/")
   end
-  xit "can return all items" do
+  it "can return all items" do
     get "/api/v1/items"
 
     items = JSON.parse(response.body)
     item = items.first
+    # binding.pry
+
+    expect(response.status).to eq(200)
     expect(items.count).to eq(2)
     expect(item).to have_key("id")
     expect(item).to have_key("name")
@@ -29,14 +32,35 @@ describe "Items API" do
 
     item = JSON.parse(response.body)
 
-    expect(item["name"]).to eq
+    expect(response.status).to eq(200)
+    expect(item["name"]).to eq("Widget")
+    expect(item["description"]).to eq("everyone needs one")
+    expect(item["image_url"]).to eq("/")
+    expect(item).not_to have_key("created_at")
+    expect(item).not_to have_key("updated_at")
   end
 
   xit "can delete one item by id" do
+    delete "/api/v1/items/#{@item_1.id}"
 
+    expect(response.status).to eq(204)
+    expect(Item.count).to eq(1)
   end
 
   xit "can create an item" do
+    item_attributes = { name: "Trinkit",
+                        description: "very shiny",
+                        image_url: "/"}
 
+    post "/api/v1/items", item: item_attributes
+
+    item = JSON.parse(response.body)
+
+    expect(response.status).to eq(201)
+    expect(item["name"]).to eq("Trinkit")
+    expect(item["description"]).to eq("very shiny")
+    expect(item["image_url"]).to eq("/")
+    expect(item).not_to have_key("created_at")
+    expect(item).not_to have_key("updated_at")
   end
 end
